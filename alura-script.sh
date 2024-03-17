2278bd35b40f9d58b5a98b3c9aca13f94b49de31
Sagaz <patrickmarques.job@hotmail.com>
2024-03-17 02:45:30 -0300

    [AULA]: Renomeando arquivos

--- alura-script.sh
+++ alura-script.sh
@@ -5,16 +5,19 @@
 from="jpg"
 to="png"
 
-IMAGE_PATH=~/repositorios/own/scripts/shell-script-alura/imagens-livros
+cd ~/repositorios/own/scripts/shell-script-alura/imagens-livros
+
 TARGET_DIRECTORY=~/repositorios/own/scripts/shell-script-alura/converted-imgs
 
-for img in $@ 
+for img in *.$from
 do 
   
-  convert $IMAGE_PATH/$img.$from  $IMAGE_PATH/$img.$to
-  
-  mv $IMAGE_PATH/$img.$to $TARGET_DIRECTORY
+  # variable=$() -> Atribui a uma variável o retorno da execução de um comando.
+  no_extension_img=$(ls $img | awk -F. '{ print $1 }');
+
+  convert $no_extension_img.$from $TARGET_DIRECTORY/$no_extension_img.$to
 
 done;
 
+ls $TARGET_DIRECTORY;
 
